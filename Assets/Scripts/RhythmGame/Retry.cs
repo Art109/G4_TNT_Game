9a1e118cb009db1a27ba601a7bafd370f6b79ca8
Gamers <limawl13@gmail.com>
2025-04-17 15:31:24 -0400

    Atualização da tela de Game Over

---
+++ Assets/Scripts/RhythmGame/Retry.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Retry : MonoBehaviour
+{
+    public GameObject fadeOut;
+    public GameObject gameOver, retry;
+    public AudioSource music;
+    public AudioSource select;
+    private bool isRetrying = false;
+    private void Start()
+    {
+        Invoke("MusicAfterDelay", 2f);
+        Invoke("GameOverAfterDelay", 2.2f);
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isRetrying == false)
+        {
+            select.Play();
+            fadeOut.SetActive(true);
+            Invoke("RetryAfterDelay", 3f);
+            isRetrying = true;
+            music.Stop();
+        }
+    }
+    void RetryAfterDelay()
+    {
+        SceneManager.LoadScene(0);
+    }
+    void MusicAfterDelay()
+    {
+        if (isRetrying == false)
+        {
+            music.Play();
+        }
+        else
+        {
+            music.Stop();
+        }
+    }
+    void GameOverAfterDelay()
+    {
+        gameOver.SetActive(true);
+        retry.SetActive(true);
+    }
+}
